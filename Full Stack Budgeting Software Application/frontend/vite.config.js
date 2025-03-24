a30bb64c446ab0b0c7d302014e8d5a585c0b3c9e
Eunice Koid <73344623+eunicekoid@users.noreply.github.com>
2025-03-24 13:04:25 -0700

    updated

--- Full Stack Budgeting Software Application/frontend/vite.config.js
+++ Full Stack Budgeting Software Application/frontend/vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/accounts': process.env.VITE_BACKEND_URL,
-//       '/categories': process.env.VITE_BACKEND_URL,
-//       '/transactions': process.env.VITE_BACKEND_URL,
-//       '/budgets': process.env.VITE_BACKEND_URL,
-//       '/subcategories': process.env.VITE_BACKEND_URL,
-//       '/wolfram': process.env.VITE_BACKEND_URL,
-//     },
-//   },
-// });
-
-
 export default defineConfig({
   plugins: [react({
     jsxRuntime: 'automatic',
